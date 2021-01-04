e107c1a790363778a30cff22ddd44b5f2bb7ce89
M1-50-mariam <76564921+M1-50-mariam@users.noreply.github.com>
2021-01-04 17:40:01 +0300

    Create BikeRentalApp.swift

---
+++ BikeRental/BikeRental/BikeRentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeRentalApp.swift
+//  BikeRental
+//
+//  Created by Mariam2 on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct BikeRentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
